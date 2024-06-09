1b1a33c2b742abb9ddc585290ca6cb5ed13d4c35
Erum Sikander <mrsikanderghouri@gmail.com>
2024-06-10 04:24:36 +0500

    add type inference

--- new.js
+++ new.js
@@ -1,14 +1,17 @@
 "use strict";
 // Sir-Zia-repo-04   practice:
 // Strong typing define:
-// nomal typing
 Object.defineProperty(exports, "__esModule", { value: true });
+//strong typing
 let student = "Erum Sikander";
 let age = 26;
-let marriad = true;
-console.log(student);
-//strong typing
-let student01 = "Erum Sikander";
-let age01 = 26;
-let married01 = true;
-console.log(student01);
+let married = true;
+console.log(student + " " + age + " " + married + " ");
+// type inference:
+let student_name = "Erum";
+student_name = "Saba";
+let student_age = 26;
+student_age = 20;
+let student_status = true;
+student_status = false;
+console.log(student_name + " " + student_age + " " + student_status + " ");
--- new.ts
+++ new.ts
@@ -2,17 +2,17 @@
 
 // Sir-Zia-repo-04   practice:
 // Strong typing define:
-// nomal typing
-
-let student = "Erum Sikander";
-let age = 26;
-let marriad = true;
-console.log(student);
-
 //strong typing
+let student : string = "Erum Sikander";
+let age: number = 26;
+let status : boolean = true;
+console.log(student + " " + age + " " + status + " " );
 
-let student01 : string = "Erum Sikander";
-let age01: number = 26;
-let married01 : boolean = true;
-
-console.log(student01);
+// type inference:
+let student_name = "Erum";
+student_name = "Saba";
+let student_age = 26;
+student_age = 20;
+let student_status = true;
+student_status = false;
+console.log(student_name + " " + student_age + " " + student_status + " " );
