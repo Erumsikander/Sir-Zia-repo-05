0b1c5dcafb5d81a04e3494700261cdade115c9b9
Erum Sikander <mrsikanderghouri@gmail.com>
2024-06-09 13:58:07 +0500

    complete strong typing.

---
+++ new.js
@@ -0,0 +1,14 @@
+"use strict";
+// Sir-Zia-repo-04   practice:
+// Strong typing define:
+// nomal typing
+Object.defineProperty(exports, "__esModule", { value: true });
+let student = "Erum Sikander";
+let age = 26;
+let marriad = true;
+console.log(student);
+//strong typing
+let student01 = "Erum Sikander";
+let age01 = 26;
+let married01 = true;
+console.log(student01);
---
+++ new.ts
@@ -0,0 +1,18 @@
+
+
+// Sir-Zia-repo-04   practice:
+// Strong typing define:
+// nomal typing
+
+let student = "Erum Sikander";
+let age = 26;
+let marriad = true;
+console.log(student);
+
+//strong typing
+
+let student01 : string = "Erum Sikander";
+let age01: number = 26;
+let married01 : boolean = true;
+
+console.log(student01);
